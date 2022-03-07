a0564145f47495ba82963aa1f550a3d2123c96ab
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-08 00:37:26 +0900

    create store

--- redux-practice/src/app.jsx
+++ redux-practice/src/app.jsx
@@ -1,32 +1,11 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {add,minus} from './store';
 
-const App = ({count,increment,decrement}) => {
+const App = () => {
   return (
     <>
-     <h1>count={count}</h1> 
-     <button onClick={increment}>+</button>
-     <button onClick={decrement}>-</button>
-
+      <h1>TodoList</h1>
     </>
   );
 };
 
-function mapStateToProps(state,ownProps) {
-  return {count : state}
-  
-}
-function mapDispatchToProps(dispatch,ownProps) {
-    return{
-      increment:()=>{
-        dispatch(add())
-      },
-      decrement:()=>{
-        dispatch(minus())
-      }
-    }
-
-}
-
-export default connect(mapStateToProps,mapDispatchToProps) (App);
+export default App;
--- redux-practice/src/store.js
+++ redux-practice/src/store.js
@@ -1,14 +1,19 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const handleCount = createSlice({
-    name:"count",
-    initialState:0,
+
+const handleList = createSlice({
+    name: 'lists',
+    initialState:[],
     reducers:{
-        add:(state,action)=>state+1,
-        minus:(state,action)=>state-1,
+        addList:(state,action)=>{
+            state.push({text:action.payload, id:Date.now()});
+        },
+        deleteList:(state,action)=>
+        state.filter((list)=>list.id!==action.payload),
     }
 })
 
-const store = configureStore({reducer:handleCount.reducer})
+
+const store = configureStore({reducer:handleList.reducer});
 export default store;
-export const {add,minus} = handleCount.actions;
+export const {addList,deleteList} = handleList.actions;
